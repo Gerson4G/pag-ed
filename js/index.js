965110013f98c428f1ad5072d9da19b0a30b1796
Gerson4G <gersongoatache@gmail.com>
2016-03-18 22:34:20 -0430

    Falta un poco el tamaño de suma y la parte extra en animales al hacer click al lado del icono audio

---
+++ js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'sonidos/introduccion.mp3');
+    audioElement.play();
+});
